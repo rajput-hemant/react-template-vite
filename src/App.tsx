9d5f7b8ee629db55c3d8af94599c053a85212f0c
rajput-hemant <rajput.hemant2001@gmail.com>
2023-06-28 12:47:52 +0530

    feat: move template w/ react-router to seperate branch
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,10 @@
-import { Suspense } from "react";
-import { Route, Routes } from "react-router-dom";
-
 import RootLayout from "./components/layout";
-import Center from "./components/ui/center";
-import Loader from "./components/ui/loader";
 import Home from "./pages/home";
 
 const App = () => {
   return (
     <RootLayout>
-      <Suspense
-        fallback={
-          <Center absolutely>
-            <Loader iconSize={50} />
-          </Center>
-        }
-      >
-        <Routes>
-          <Route index path="/" element={<Home />} />
-
-          {/* ... */}
-        </Routes>
-      </Suspense>
+      <Home />
     </RootLayout>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter as Router } from "react-router-dom";
 
 import App from "./App.tsx";
 
@@ -8,8 +7,6 @@ import "@/styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <App />
   </React.StrictMode>
 );
