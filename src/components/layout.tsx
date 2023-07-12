c21ce93054ca31a7c8847726f1a0930bc9aef502
rajput-hemant <rajput.hemant2001@gmail.com>
2023-07-12 20:42:54 +0530

    feat: improve feature cards
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -4,7 +4,7 @@ import "@/styles/layout.css";
 
 const RootLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="layout min-h-screen bg-black/90 p-8 text-white antialiased lg:p-16">
+    <div className="layout min-h-screen bg-black/90 px-8 pt-4 text-white antialiased lg:px-16">
       {children}
     </div>
   );
