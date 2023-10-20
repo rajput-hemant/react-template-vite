802dce2c075944e172aea13634755aa7837c1a9f
rajput-hemant <rajput.hemant2001@gmail.com>
2023-10-20 13:09:11 +0530

    feat: update template homepage
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,9 @@
-import React from "react";
+type Props = {
+  children: React.ReactNode;
+};
 
-import "@/styles/layout.css";
-
-const RootLayout = ({ children }: { children: React.ReactNode }) => {
-  return (
-    <div className="layout min-h-screen bg-black/90 px-8 pt-4 text-white antialiased lg:px-16">
-      {children}
-    </div>
-  );
+const RootLayout = ({ children }: Props) => {
+  return <div className="scroll-smooth antialiased">{children}</div>;
 };
 
 export default RootLayout;
