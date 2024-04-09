5df994c20190a3cba4f892e1a56cd16885f7c7a0
rajput-hemant <rajput.hemant2001@gmail.com>
2024-04-09 20:43:46 +0530

    refactor: ran lint --fix
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/routes/index.lazy.tsx
+++ src/routes/index.lazy.tsx
@@ -778,7 +778,7 @@ function Home() {
               rel="noopener noreferrer"
               className="card group relative h-48 w-full rounded-xl bg-zinc-700 shadow-md shadow-black outline-none before:absolute before:left-0 before:top-0 before:size-full before:rounded-xl before:opacity-0 before:transition-opacity before:duration-500 after:absolute after:left-0 after:top-0 after:size-full after:rounded-xl after:opacity-0 after:transition-opacity after:duration-500 hover:shadow-xl hover:shadow-black hover:before:opacity-100"
             >
-              <div className="absolute inset-[1px] z-[2] flex flex-col gap-2.5 rounded-xl bg-[#141414] p-2.5">
+              <div className="absolute inset-px z-[2] flex flex-col gap-2.5 rounded-xl bg-[#141414] p-2.5">
                 <div className="relative size-full overflow-hidden rounded-md p-4">
                   <div className="mb-3.5 size-14">{<f.logo />}</div>
 
