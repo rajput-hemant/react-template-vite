610f3e5fdee7b84b67be91b7a76e9c992d9e8234
rajput-hemant <rajput.hemant2001@gmail.com>
2024-06-11 05:27:44 +0530

    feat: migrate to eslint flatConfig
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/hooks/use-event-listener.ts
+++ src/hooks/use-event-listener.ts
@@ -1,4 +1,6 @@
-import { useEffect, useRef, type RefObject } from "react";
+import { useEffect, useRef } from "react";
+
+import type { RefObject } from "react";
 
 import { useIsomorphicLayoutEffect } from "./use-isomorphic-layout-effect";
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { fileURLToPath } from "url";
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
 
