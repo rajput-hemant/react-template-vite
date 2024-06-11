b656ee9365cab1ed844cee269cf31fad32414496
rajput-hemant <rajput.hemant2001@gmail.com>
2024-06-11 05:37:34 +0530

    feat: migrate to eslint flatConfig
    Signed-off-by: rajput-hemant <rajput.hemant2001@gmail.com>

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,6 +1,7 @@
 import "@/styles/layout.css";
 
 import React from "react";
+
 import {
   Clipboard,
   ClipboardCheck,
@@ -48,11 +49,11 @@ const features: Feature[] = [
         height="48"
         viewBox="0 0 48 48"
       >
-        <rect width="36" height="36" x="6" y="6" fill="#1976d2"></rect>
+        <rect width="36" height="36" x="6" y="6" fill="#1976d2" />
         <polygon
           fill="#fff"
           points="27.49,22 14.227,22 14.227,25.264 18.984,25.264 18.984,40 22.753,40 22.753,25.264 27.49,25.264"
-        ></polygon>
+        />
         <path
           fill="#fff"
           d="M39.194,26.084c0,0-1.787-1.192-3.807-1.192s-2.747,0.96-2.747,1.986 c0,2.648,7.381,2.383,7.381,7.712c0,8.209-11.254,4.568-11.254,4.568V35.22c0,0,2.152,1.622,4.733,1.622s2.483-1.688,2.483-1.92 c0-2.449-7.315-2.449-7.315-7.878c0-7.381,10.658-4.469,10.658-4.469L39.194,26.084z"
@@ -152,207 +153,166 @@ const features: Feature[] = [
               <g id="Group">
                 <path
                   id="Rectangle"
-                  className="st0"
                   d="M155 40h10c2.8 0 5 2.2 5 5s-2.2 5-5 5h-10c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_1_"
-                  className="st1"
                   d="M5 200h50c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_2_"
-                  className="st2"
                   d="M125 120h30c2.8 0 5 2.2 5 5s-2.2 5-5 5h-30c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_3_"
-                  className="st1"
                   d="M65 120h40c2.8 0 5 2.2 5 5s-2.2 5-5 5H65c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_4_"
-                  className="st0"
                   d="M5 120h40c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_5_"
-                  className="st2"
                   d="M5 160h50c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_6_"
-                  className="st2"
                   d="M5 80h50c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_7_"
-                  className="st3"
                   d="M55 20h100c2.8 0 5 2.2 5 5s-2.2 5-5 5H55c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_8_"
-                  className="st1"
                   d="M5 20h30c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_9_"
-                  className="st3"
                   d="M45 180h10c2.8 0 5 2.2 5 5s-2.2 5-5 5H45c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_10_"
-                  className="st0"
                   d="M45 60h10c2.8 0 5 2.2 5 5s-2.2 5-5 5H45c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_11_"
-                  className="st0"
                   d="M5 180h20c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_12_"
-                  className="st3"
                   d="M5 60h20c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_13_"
-                  className="st4"
                   d="M135 180h10c2.8 0 5 2.2 5 5s-2.2 5-5 5h-10c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_14_"
-                  className="st4"
                   d="M75 180h40c2.8 0 5 2.2 5 5s-2.2 5-5 5H75c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_15_"
-                  className="st4"
                   d="M165 180h40c2.8 0 5 2.2 5 5s-2.2 5-5 5h-40c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_16_"
-                  className="st0"
                   d="M85 100h80c2.8 0 5 2.2 5 5s-2.2 5-5 5H85c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_17_"
-                  className="st3"
                   d="M35 100h30c2.8 0 5 2.2 5 5s-2.2 5-5 5H35c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_18_"
-                  className="st1"
                   d="M5 100h10c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_19_"
-                  className="st2"
                   d="M95 40h40c2.8 0 5 2.2 5 5s-2.2 5-5 5H95c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_20_"
-                  className="st0"
                   d="M5 40h70c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_21_"
-                  className="st3"
                   d="M35 140h90c2.8 0 5 2.2 5 5s-2.2 5-5 5H35c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_22_"
-                  className="st2"
                   d="M5 140h10c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_23_"
-                  className="st1"
                   d="M125 60h50c2.8 0 5 2.2 5 5s-2.2 5-5 5h-50c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_24_"
-                  className="st3"
                   d="M125 80h50c2.8 0 5 2.2 5 5s-2.2 5-5 5h-50c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_25_"
-                  className="st0"
                   d="M5 0h120c2.8 0 5 2.2 5 5s-2.2 5-5 5H5c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_26_"
-                  className="st4"
                   d="M145 0h60c2.8 0 5 2.2 5 5s-2.2 5-5 5h-60c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_27_"
-                  className="st4"
                   d="M145 140h20c2.8 0 5 2.2 5 5s-2.2 5-5 5h-20c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_28_"
-                  className="st4"
                   d="M185 140h20c2.8 0 5 2.2 5 5s-2.2 5-5 5h-20c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_29_"
-                  className="st4"
                   d="M175 120h30c2.8 0 5 2.2 5 5s-2.2 5-5 5h-30c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_30_"
-                  className="st4"
                   d="M175 20h30c2.8 0 5 2.2 5 5s-2.2 5-5 5h-30c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_31_"
-                  className="st4"
                   d="M185 100h20c2.8 0 5 2.2 5 5s-2.2 5-5 5h-20c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_32_"
-                  className="st4"
                   d="M185 40h20c2.8 0 5 2.2 5 5s-2.2 5-5 5h-20c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_33_"
-                  className="st4"
                   d="M195 60h10c2.8 0 5 2.2 5 5s-2.2 5-5 5h-10c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_34_"
-                  className="st4"
                   d="M195 80h10c2.8 0 5 2.2 5 5s-2.2 5-5 5h-10c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_35_"
-                  className="st4"
                   d="M115 160h90c2.8 0 5 2.2 5 5s-2.2 5-5 5h-90c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_36_"
-                  className="st4"
                   d="M75 160h20c2.8 0 5 2.2 5 5s-2.2 5-5 5H75c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_37_"
-                  className="st4"
                   d="M155 200h50c2.8 0 5 2.2 5 5s-2.2 5-5 5h-50c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_38_"
-                  className="st4"
                   d="M75 200h60c2.8 0 5 2.2 5 5s-2.2 5-5 5H75c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_39_"
-                  className="st4"
                   d="M75 60h30c2.8 0 5 2.2 5 5s-2.2 5-5 5H75c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
                 <path
                   id="Rectangle_40_"
-                  className="st4"
                   d="M75 80h30c2.8 0 5 2.2 5 5s-2.2 5-5 5H75c-2.8 0-5-2.2-5-5s2.2-5 5-5z"
                 />
               </g>
@@ -690,11 +650,9 @@ export default function Home() {
                   onClick={() => setShowDropdown(!showDropdown)}
                   className="text-zinc-400 transition-colors hover:text-white"
                 >
-                  {isCopied ? (
+                  {isCopied ?
                     <ClipboardCheck className="size-5" />
-                  ) : (
-                    <Clipboard className="size-5" />
-                  )}
+                  : <Clipboard className="size-5" />}
                 </button>
               </p>
 
@@ -774,7 +732,9 @@ export default function Home() {
             >
               <div className="absolute inset-px z-[2] flex flex-col gap-2.5 rounded-xl bg-[#141414] p-2.5">
                 <div className="relative size-full overflow-hidden rounded-md p-4">
-                  <div className="mb-3.5 size-14">{<f.logo />}</div>
+                  <div className="mb-3.5 size-14">
+                    <f.logo />
+                  </div>
 
                   <h3 className="text-xl">
                     <div className="flex size-full items-center after:absolute after:inset-0">
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,5 @@
 import { fileURLToPath } from "url";
+
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
